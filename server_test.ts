917bbd24255eeda0ee27259ff55b83284693c89b
magnattic <atticae@gmail.com>
2021-02-04 15:45:04 +0100

    disable watch in tests

--- server_test.ts
+++ server_test.ts
@@ -9,31 +9,31 @@ const assertJSON = async (actual: Response, expected: unknown) => {
   assertEquals(await actual.json(), expected);
 };
 
-// test({
-//   name: 'serve a whole json db',
-//   fn: async () => {
-//     const aborter = new AbortController();
-//     const db = await loadDatabase('./example/db.json', {
-//       signal: aborter.signal,
-//     });
-//     const server = await jsonServer({
-//       dbPathOrObject: './example/db.json',
-//       port: 8001,
-//       watchDB: false,
-//     });
+test({
+  name: 'serve a whole json db',
+  fn: async () => {
+    const aborter = new AbortController();
+    const db = await loadDatabase('./example/db.json', {
+      signal: aborter.signal,
+    });
+    const server = await jsonServer({
+      dbPathOrObject: './example/db.json',
+      port: 8001,
+      watchDB: false,
+    });
 
-//     const response = await fetch('http://localhost:8001');
+    const response = await fetch('http://localhost:8001');
 
-//     await assertJSON(response, db);
-//     server.close();
-//   },
-// });
+    await assertJSON(response, db);
+    server.close();
+  },
+});
 
 test({
   name: 'load a json object db',
   fn: async () => {
     const db = { test: 'epic' };
-    const server = await jsonServer({ dbPathOrObject: db });
+    const server = await jsonServer({ dbPathOrObject: db, watchDB: false });
 
     const response = await fetch('http://localhost:8000');
 
@@ -45,7 +45,10 @@ test({
 test({
   name: 'serve a route from json file',
   fn: async () => {
-    const server = await jsonServer({ dbPathOrObject: './example/db.json' });
+    const server = await jsonServer({
+      dbPathOrObject: './example/db.json',
+      watchDB: false,
+    });
     const response = await fetch('http://localhost:8000/profile');
 
     await assertJSON(response, { user: 'magnattic' });
@@ -62,7 +65,7 @@ test({
         { id: 2, title: 'Alice in Denoland' },
       ],
     };
-    const server = await jsonServer({ dbPathOrObject: db });
+    const server = await jsonServer({ dbPathOrObject: db, watchDB: false });
 
     const response = await fetch('http://localhost:8000/posts/2');
 
